0bca32b0e8204ee635f7a409ff934e714c35efe9
Percy Mamedy <percymamedy@gmail.com>
2022-07-16 19:59:56 +0400

    first commit

---
+++ src/TetherServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Interfaceable\Tether;
+
+use Illuminate\Support\ServiceProvider;
+
+class TetherServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+    }
+}
